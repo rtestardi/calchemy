d341be9d5549bd97bc3fdbb0c111f8feec706a86
Richard Testardi <richardt@microsoft.com>
2020-04-29 17:41:53 -0600

    more refactor

--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,3 @@
 $db = gc database_dev.txt
-gc calchemy_dev.html | %{ if ($_ -match "database_dev.txt") { "<script>"; $db } else { $_ } } | sc calchemy.html
+$e = gc engine_dev.js
+gc calchemy_dev.html | %{ if ($_ -match "database_dev.txt") { "<script>"; $db } elseif ($_ -match "engine_dev.js") { "<script>"; $e } else { $_ } } | sc calchemy.html
