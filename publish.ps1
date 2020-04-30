ae8f5e635dfab34cd789826b15aed4d32227fe3a
Rich Testardi <rtestardi@live.com>
2020-04-29 23:19:44 -0600

    factor out engine and database js from calchemy html and js (#62)

--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,3 @@
-$db = gc database_dev.txt
-gc calchemy_dev.html | %{ if ($_ -match "database_dev.txt") { "<script>"; $db } else { $_ } } | sc calchemy.html
+$db = gc database_dev.js
+$e = gc calchemy_dev.js
+gc calchemy_dev.html | %{ if ($_ -match "database_dev.js") { "<script>"; $db } elseif ($_ -match "calchemy_dev.js") { "<script>"; $e } else { $_ } } | sc calchemy.html
