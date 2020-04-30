e9b4850da5526794a442f1c8067cdb154a70988c
Rich Testardi <rtestardi@live.com>
2020-04-29 23:30:04 -0600

    separate html from js (#64)

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,3 @@
 $db = gc database_dev.js
-$e = gc calchemy_dev.js
-gc calchemy_dev.html | %{ if ($_ -match "database_dev.js") { "<script>"; $db } elseif ($_ -match "calchemy_dev.js") { "<script>"; $e } else { $_ } } | sc calchemy.html
+$e = gc engine_dev.js
+gc calchemy_dev.html | %{ if ($_ -match "database_dev.js") { "<script>"; $db } elseif ($_ -match "engine_dev.js") { "<script>"; $e } else { $_ } } | sc calchemy.html
