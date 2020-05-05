7430e5aad5b9a428242b948bb3a0c64e8ea525b0
Rich Testardi <rtestardi@live.com>
2020-05-05 06:31:39 -0600

    more (#110)
    Co-authored-by: Richard Testardi <richardt@microsoft.com>

--- engine_dev.js
+++ engine_dev.js
@@ -1351,8 +1351,8 @@ function ParseTokens(tokens, line)
                             // N.B. we use index 1 for PREFIX solo
                             unit.interpretation = names[1];
                         } else {
-                            // XXX -- for now we use the longer of names[0, 1, 2], with a preference for names[1] if they are equal
-                            if (unit.names.length > 1 && unit.names[1].length >= unit.names[0].length) {
+                            // XXX -- for now we use the longer of names[0, 1, 2], with a preference for names[1] if they are equal and don't only differ by case
+                            if (unit.names.length > 1 && unit.names[1].length >= unit.names[0].length && unit.names[1].toLowerCase() != unit.names[0].toLowerCase()) {
                                 unit.interpretation = names[1];
                                 if (unit.names.length > 2 && unit.names[2].length > unit.names[1].length) {
                                     unit.interpretation = names[2];
