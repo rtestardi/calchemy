8ec39e14bb41845bc849c7eb22d505d095167d22
Rich Testardi <rtestardi@live.com>
2020-06-06 14:23:25 -0600

    update databases (#181)
    Co-authored-by: Richard Testardi <richardt@microsoft.com>

--- elements.js
+++ elements.js
@@ -1,6 +1,10 @@
 LoadRuntimeDatabase("elements.js", `
 CATEGORY elements
 
+___PUSH elements
+
+___POP elements
+
 TEST 1|= 1
 TEST 1|= 1
 TEST 1|= 1
--- solarsys.js
+++ solarsys.js
@@ -1,48 +1,52 @@
 LoadRuntimeDatabase("solarsys.js", `
 CATEGORY solarsys
 
-solarsys:sun_mass = 1.99E+30 kilogramm #mass of the sun
-solarsys:sun_diam = 1.39E+09 meter #mean diameter of the sun
+___PUSH solarsys
 
-solarsys:moon_mass = 7.36E+22 kilogramm #mass of the moon
-solarsys:moon_diam = 3.48E+06 meter #mean diameter of moon
-solarsys:moon_dist = 3.85E+08 meter #mean distance earth to moon
+sun_mass = 1.99E+30 kilogramm #mass of the sun
+sun_diam = 1.39E+09 meter #mean diameter of the sun
 
-solarsys:mercury_mass = 3.25E+23 kilogramm #mass of mercury
-solarsys:mercury_diam = 5140 kilometer #mean diameter of mercury
-solarsys:mercury_dist = 5.79E+10 meter #mean distance mercury to sun
+moon_mass = 7.36E+22 kilogramm #mass of the moon
+moon_diam = 3.48E+06 meter #mean diameter of moon
+moon_dist = 3.85E+08 meter #mean distance earth to moon
 
-solarsys:venus_mass = 4.86E+24 kilogramm #mass of venus
-solarsys:venus_diam = 12620 kilometer #mean diameter of venus
-solarsys:venus_dist = 1.08E+11 meter #mean distance venus to sun
+mercury_mass = 3.25E+23 kilogramm #mass of mercury
+mercury_diam = 5140 kilometer #mean diameter of mercury
+mercury_dist = 5.79E+10 meter #mean distance mercury to sun
 
-solarsys:earth_mass = 5.975E+24 kilogramm #mass of the earth
-solarsys:earth_diam = 1.2756E+07 meter #mean diameter of the earth
-solarsys:earth_dist = 1.496E+11 meter #mean distance earth to sun
+venus_mass = 4.86E+24 kilogramm #mass of venus
+venus_diam = 12620 kilometer #mean diameter of venus
+venus_dist = 1.08E+11 meter #mean distance venus to sun
 
-solarsys:mars_mass = 6.33E+23 kilogramm #mass of mars
-solarsys:mars_diam = 6860 kilometer #mean diameter of mars
-solarsys:mars_dist = 2.28E+11 meter #mean distance mars to sun
+earth_mass = 5.975E+24 kilogramm #mass of the earth
+earth_diam = 1.2756E+07 meter #mean diameter of the earth
+earth_dist = 1.496E+11 meter #mean distance earth to sun
 
-solarsys:jupiter_mass = 1.88E+27 kilogramm #mass of jupiter
-solarsys:jupiter_diam = 143600 kilometer #mean diameter of jupiter
-solarsys:jupiter_dist = 7.78E+11 meter #mean distance jupiter to sun
+mars_mass = 6.33E+23 kilogramm #mass of mars
+mars_diam = 6860 kilometer #mean diameter of mars
+mars_dist = 2.28E+11 meter #mean distance mars to sun
 
-solarsys:saturn_mass = 5.62E+26 kilogramm #mass of saturn
-solarsys:saturn_diam = 120600 kilometer #mean diameter of saturn
-solarsys:saturn_dist = 1.43E+12 meter #mean distance saturn to sun
+jupiter_mass = 1.88E+27 kilogramm #mass of jupiter
+jupiter_diam = 143600 kilometer #mean diameter of jupiter
+jupiter_dist = 7.78E+11 meter #mean distance jupiter to sun
 
-solarsys:uranus_mass = 8.60E+25 kilogramm #mass of uranus
-solarsys:uranus_diam = 53400 kilometer #mean diameter of uranus
-solarsys:uranus_dist = 2.87E+12 meter #mean distance uranus to sun
+saturn_mass = 5.62E+26 kilogramm #mass of saturn
+saturn_diam = 120600 kilometer #mean diameter of saturn
+saturn_dist = 1.43E+12 meter #mean distance saturn to sun
 
-solarsys:neptune_mass = 9.98E+25 kilogramm #mass of neptune
-solarsys:neptune_diam = 49700 kilometer #mean diameter of neptune
-solarsys:neptune_dist = 4.50E+12 meter #mean distance neptune to sun
+uranus_mass = 8.60E+25 kilogramm #mass of uranus
+uranus_diam = 53400 kilometer #mean diameter of uranus
+uranus_dist = 2.87E+12 meter #mean distance uranus to sun
 
-solarsys:pluto_mass = 5.9E+24 kilogramm #mass of pluto
-solarsys:pluto_diam = 12700 kilometer #mean diameter of pluto
-solarsys:pluto_dist = 5.91E+12 meter #mean distance pluto to sun
+neptune_mass = 9.98E+25 kilogramm #mass of neptune
+neptune_diam = 49700 kilometer #mean diameter of neptune
+neptune_dist = 4.50E+12 meter #mean distance neptune to sun
+
+pluto_mass = 5.9E+24 kilogramm #mass of pluto
+pluto_diam = 12700 kilometer #mean diameter of pluto
+pluto_dist = 5.91E+12 meter #mean distance pluto to sun
+
+___POP solarsys
 
 TEST 1|= 1
 TEST 1|= 1
