5c54e3a8e4417645bf4a9da2f0622f692d398209
Rich Testardi <rtestardi@live.com>
2020-06-16 19:17:16 -0600

    offset cleanup (#187)

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,4 @@
 $db = gc database_dev.js
 $e = gc engine_dev.js
 gc calchemy_dev.html | %{ if ($_ -match "database_dev.js") { "<script>"; $db } elseif ($_ -match "engine_dev.js") { "<script>"; $e } else { $_ } } | sc calchemy.html
+gc calchemy.html | ?{ $_.length -gt 178 }
