020b01559352abd1fed32cde192f9f9a95248ad0
Richard Testardi <richardt@microsoft.com>
2020-11-28 10:33:55 -0700

    find units by substring, new database from ken

--- engine_dev.js
+++ engine_dev.js
@@ -1003,7 +1003,7 @@ function Singulars(name)
 function MatchPrefixPlural(name, i, j, search)
 {
     var singular = units[i].names[j];
-    if (name == singular || (search && singular.toLowerCase().match("^" + name))) {
+    if (name == singular || (search && singular.toLowerCase().match("^" + name + "|_" + name))) {
         return true;
     }
     if (units[i].pluralizables == null || ! units[i].pluralizables[j] || name[name.length-1] != 's' || search) {
