c922d563fa08151ba3b6edeff4d54df7d0cb45cf
Rich Testardi <rtestardi@live.com>
2026-01-03 15:09:45 -0500

    more

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,10 @@
+param([switch]$exp)
 $db = gc database_dev.js
 $e = gc engine_dev.js
-gc calchemy_dev.html | %{ if ($_ -match "database_dev.js") { "<script>"; $db } elseif ($_ -match "engine_dev.js") { "<script>"; $e } else { $_ } } | sc calchemy.html
-gc calchemy.html | ?{ $_.length -gt 178 }
+if ($exp) {
+  $out = "calchemy.exp.html"
+} else {
+  $out = "calchemy.html"
+}
+gc calchemy_dev.html | %{ if ($_ -match "database_dev.js") { "<script>"; $db } elseif ($_ -match "engine_dev.js") { "<script>"; $e } else { $_ } } | sc $out
+gc $out | ?{ $_.length -gt 178 }
